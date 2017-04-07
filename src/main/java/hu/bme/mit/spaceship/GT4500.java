ef75bc0866b3f92f4ce5a3981798359737ebb2c1
tothpeter37 <tothpeter37@sch.bme.hu>
2017-04-06 23:48:08 -0700

    Fixed bug, where second torpedo doesn't fire if the first one fails.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,8 +78,10 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedos
-          boolean success = false;
-        firingSuccess = fireTorpedos(FiringMode.SINGLE) && fireTorpedos(FiringMode.SINGLE);
+        boolean success = false;
+          boolean t1  = fireTorpedos(FiringMode.SINGLE);
+          boolean t2  = fireTorpedos(FiringMode.SINGLE);
+        firingSuccess = t1 && t2;
         break;
     }
 
